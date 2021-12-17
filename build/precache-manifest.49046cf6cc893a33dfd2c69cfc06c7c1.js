53f1134d0c3aabe9a3749cbb4236e3e35e6d748d
Zhili Feng <fzl.007@gmail.com>
2021-12-17 00:02:01 -0500

    d

---
+++ build/precache-manifest.49046cf6cc893a33dfd2c69cfc06c7c1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "034c56c3db5ea8056cb6",
+    "url": "/static/js/main.034c56c3.chunk.js"
+  },
+  {
+    "revision": "8e2cbb4189e3aaf8361f",
+    "url": "/static/js/1.8e2cbb41.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e1cb8d21bffbcc2a8c7bfcf95bad12ff",
+    "url": "/static/media/walletconnect-banner.e1cb8d21.png"
+  },
+  {
+    "revision": "906078994f7221a25b900f47425b3ecc",
+    "url": "/static/media/erc20.90607899.svg"
+  },
+  {
+    "revision": "42bca7317690c3f5e9ecd8db387344df",
+    "url": "/static/media/eth.42bca731.svg"
+  },
+  {
+    "revision": "e37299a879ceb5a23000d8a2bb10bc46",
+    "url": "/static/media/xdai.e37299a8.png"
+  },
+  {
+    "revision": "afba23b6b724896ef8b94f8d483b46e1",
+    "url": "/index.html"
+  }
+];
